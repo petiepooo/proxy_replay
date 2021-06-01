7a37e6483abd1e2ce87516490835b9a9390603f1
Pete Nelson <pete.nelson@unitedlex.com>
2021-06-01 08:27:04 -0400

    wip: parse packet to extract IPs and ports

--- proxy_replay.c
+++ proxy_replay.c
@@ -14,6 +14,7 @@
 #include <unistd.h>
 #include <string.h> 
 #include <assert.h>
+#include <netinet/in.h>
 
 #include <pcap.h>
 
@@ -25,12 +26,57 @@
 #define MAX_PATH_LEN 1024
 #define MAX_FILTER_LEN 1024
 
+#define SIZE_ETHERNET 14
+#define ETHER_ADDR_LEN	6
 
+struct sniff_ethernet {
+    u_char ether_dhost[ETHER_ADDR_LEN]; /* Destination host address */
+    u_char ether_shost[ETHER_ADDR_LEN]; /* Source host address */
+    u_short ether_type; /* IP? ARP? RARP? etc */
+};
+
+struct sniff_ip {
+    u_char ip_vhl;		/* version << 4 | header length >> 2 */
+    u_char ip_tos;		/* type of service */
+    u_short ip_len;		/* total length */
+    u_short ip_id;		/* identification */
+    u_short ip_off;		/* fragment offset field */
+#define IP_RF 0x8000		/* reserved fragment flag */
+#define IP_DF 0x4000		/* don't fragment flag */
+#define IP_MF 0x2000		/* more fragments flag */
+#define IP_OFFMASK 0x1fff	/* mask for fragmenting bits */
+    u_char ip_ttl;		/* time to live */
+    u_char ip_p;		/* protocol */
+    u_short ip_sum;		/* checksum */
+    struct in_addr ip_src,ip_dst; /* source and dest address */
+};
+#define IP_HL(ip)		(((ip)->ip_vhl) & 0x0f)
+#define IP_V(ip)		(((ip)->ip_vhl) >> 4)
+
+typedef u_int tcp_seq;
+
+struct sniff_tcp {
+    u_short th_sport;	/* source port */
+    u_short th_dport;	/* destination port */
+    tcp_seq th_seq;	/* sequence number */
+    tcp_seq th_ack;	/* acknowledgement number */
+    u_char th_offx2;	/* data offset, rsvd */
+#define TH_OFF(th)	(((th)->th_offx2 & 0xf0) >> 4)
+    u_char th_flags;
 #define TH_FIN 0x01
 #define TH_SYN 0x02
 #define TH_RST 0x04
 #define TH_PUSH 0x08
 #define TH_ACK 0x10
+#define TH_URG 0x20
+#define TH_ECE 0x40
+#define TH_CWR 0x80
+#define TH_FLAGS (TH_FIN|TH_SYN|TH_RST|TH_ACK|TH_URG|TH_ECE|TH_CWR)
+    u_short th_win;	/* window */
+    u_short th_sum;	/* checksum */
+    u_short th_urp;	/* urgent pointer */
+};
+
 
 struct ipv4_tuple {
     uint32_t src_ipv4;
@@ -155,8 +201,30 @@ void hash_pkt_tuple(const u_char* pkt, struct ipv4_tuple* tuple)
     uint16_t src_port = 0;
     uint16_t dst_port = 0;
 
+    struct sniff_ethernet* ethernet;
+    struct sniff_ip* ip;
+    struct sniff_tcp* tcp;
+    u_char *payload;
+    int size_ip;
+    int size_tcp;
+
     memset(tuple, 0, sizeof(struct ipv4_tuple));
+
     /* extract IPs and ports */
+    ethernet = (struct sniff_ethernet*)(pkt);
+    ip = (struct sniff_ip*)(pkt + SIZE_ETHERNET);
+    size_ip = IP_HL(ip)*4;
+    if (size_ip < 20) {
+        printf("   * Invalid IP header length: %u bytes\n", size_ip);
+        return;
+    }
+    tcp = (struct sniff_tcp*)(pkt + SIZE_ETHERNET + size_ip);
+    size_tcp = TH_OFF(tcp)*4;
+    if (size_tcp < 20) {
+        printf("   * Invalid TCP header length: %u bytes\n", size_tcp);
+        return;
+    }
+    payload = (u_char *)(pkt + SIZE_ETHERNET + size_ip + size_tcp);
 
     if(src_ipv4 < dst_ipv4)
     {
